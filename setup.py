d3f3710987e409851c5ae2734575c46a5714f508
Kim Wittenburg <codello@wittenburg.kim>
2021-06-21 22:49:32 +0200

    Bump Version Number

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude,
 
 setup(
     name='docassemble.lmrhh',
-    version='1.1.0',
+    version='1.1.1',
     description=__doc__,
     long_description=long_description(),
     long_description_content_type='text/markdown',
